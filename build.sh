eefb81112a99bf18b2a5e0e01f7f9dba1465ef31
btactic-translate-2 <btactic-translate-2@btactic.com>
2016-02-04 10:06:57 +0100

    Simplified the build script

--- build.sh
+++ build.sh
@@ -18,55 +18,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see http://www.gnu.org/licenses/.
 
-# This script does some checks and creates a zip file for zimlet release
+cd tk_barrydegraaff_owncloud_zimlet
+zip -r ../tk_barrydegraaff_owncloud_zimlet.zip *
 
-# check number of parameters in command
-PARAMS=1
-if [ $# -ne "$PARAMS" ]
-then
-  echo
-  echo "Please specify version number, for example: 1.2.3"
-  echo
-  exit 1
-fi
-
-
-
-if grep --quiet $1 README.md; then
-  echo "OK version number found in README.md" 
-else
-  echo "FAIL version number not found in README.md" 
-  exit 0
-fi
-
-if grep --quiet $1 tk_barrydegraaff_owncloud_zimlet/tk_barrydegraaff_owncloud_zimlet.xml; then
-  echo "OK version number found in tk_barrydegraaff_owncloud_zimlet/tk_barrydegraaff_owncloud_zimlet.xml" 
-else
-  echo "FAIL version number not found in tk_barrydegraaff_owncloud_zimlet/tk_barrydegraaff_owncloud_zimlet.xml" 
-  exit 0
-fi
-
-echo "Make a copy of the source"
-rm -Rf /tmp/tk_barrydegraaff_owncloud_zimlet
-mkdir /tmp/tk_barrydegraaff_owncloud_zimlet
-cp -r -v tk_barrydegraaff_owncloud_zimlet /tmp/
-
-echo "Remove _dev prefix"
-grep -lZr -e "_dev/" "/tmp/tk_barrydegraaff_owncloud_zimlet/" | xargs -0 sed -i "s^_dev/^^g"
-
-echo "Build release zip"
-cd /tmp/tk_barrydegraaff_owncloud_zimlet
-rm -Rf /tmp/tk_barrydegraaff_owncloud_zimlet/lang
-zip -r tk_barrydegraaff_owncloud_zimlet.zip *
-
-rm /tmp/tk_barrydegraaff_owncloud_zimlet.zip
-mv /tmp/tk_barrydegraaff_owncloud_zimlet/tk_barrydegraaff_owncloud_zimlet.zip /tmp/tk_barrydegraaff_owncloud_zimlet.zip
-rm -Rf /tmp/tk_barrydegraaff_owncloud_zimlet
-
-echo 'All done, your release zip should be here: /tmp/tk_barrydegraaff_owncloud_zimlet.zip'
-
+echo 'An installable version of zimlet was created on: tk_barrydegraaff_owncloud_zimlet.zip'
 
 exit 0
-
-
 
