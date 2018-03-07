9fcd924343d1bafbda1f2cd21cc2d828732126b3
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2018-03-07 17:18:33 +0100

    Changed URLs to bTactic ones so that we are independent of Zimbra-Community urls

--- webdav-client-installer.sh
+++ webdav-client-installer.sh
@@ -23,11 +23,11 @@ DOCCONVERT_ZIMLET_PRODUCTION_PATH="/opt/zimbra/zimlets-deployed/tk_barrydegraaff
 DOCCONVERT_ZIMLET_DEV_PATH="/opt/zimbra/zimlets-deployed/_dev/tk_barrydegraaff_docconvert"
 OWNCLOUD_EXTENSION_PATH="/opt/zimbra/lib/ext/ownCloud"
 DOCCONVERT_EXTENSION_PATH="/opt/zimbra/lib/ext/DocConvert"
-OWNCLOUD_ZIMLET_CLONE_URL="git://github.com/Zimbra-Community/owncloud-zimlet"
-OWNCLOUD_ZIMLET_CLONE_BRANCH="soapServiceBarry"
-OCS_JAR_URL="https://github.com/Zimbra-Community/OCS/raw/master/extension/out/artifacts/OCS_jar/OCS.jar"
-PROPMIGR_JAR_URL="https://github.com/Zimbra-Community/propmigr/raw/master/out/artifacts/propmigr_jar/propmigr.jar"
-PROP2XML_JAR_URL="https://github.com/Zimbra-Community/prop2xml/raw/master/out/artifacts/prop2xml_jar/prop2xml.jar"
+OWNCLOUD_ZIMLET_CLONE_URL="git://github.com/btactic/owncloud-zimlet"
+OWNCLOUD_ZIMLET_CLONE_BRANCH="btactic-2018"
+OCS_JAR_URL="https://github.com/btactic/OCS/raw/master/extension/out/artifacts/OCS_jar/OCS.jar"
+PROPMIGR_JAR_URL="https://github.com/btactic/propmigr/raw/master/out/artifacts/propmigr_jar/propmigr.jar"
+PROP2XML_JAR_URL="https://github.com/btactic/prop2xml/raw/master/out/artifacts/prop2xml_jar/prop2xml.jar"
 OCS_EXTENSION_PATH="/opt/zimbra/lib/ext/OCS"
 OWNCLOUD_EXTENSION_JAR_FILES="\
 ant-1.7.0.jar \
@@ -54,7 +54,7 @@ jackson-core-2.9.4.jar \
 jackson-databind-2.9.4.jar \
 java-jwt-3.3.0.jar \
 "
-OWNCLOUD_DOC_URL="https\://barrydegraaff.github.io/owncloud/"
+OWNCLOUD_DOC_URL="https\://btactic.github.io/barry-doc/owncloud/"
 
 IS_AUTO="NO"
 
